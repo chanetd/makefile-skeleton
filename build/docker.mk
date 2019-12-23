2e73b1b187049e4735f2ba49952e685b873d1750
Dominique Chanet <dominique.chanet@klarrio.com>
2019-12-23 20:28:13 +0100

    make docker builds optional

--- build/docker.mk
+++ build/docker.mk
@@ -2,19 +2,34 @@
 
 REPO?=$(shell cat REPO)
 
-.PHONY: docker
-docker: docker-push ;
+_conf_docker := $(shell [ -f ./Dockerfile ] && echo y || echo n)
 
-.PHONY: docker-build 
-docker-build: docker-prerequisites
+.PHONY: docker docker-build docker-push
+docker: docker-$(_conf_docker)
+docker-build: docker-build-$(_conf_docker)
+docker-push: docker-push-$(_conf_docker)
+
+.PHONY: docker-y
+docker-y: docker-push-y
+
+.PHONY: docker-n
+docker-n:
+	@echo "No Dockerfile in root directory -- not building or pushing a Docker container"
+
+.PHONY: docker-build-y 
+docker-build-y: docker-prerequisites-y
 	docker build -t ${REPO}:${VERSION} .
 
-.PHONY: docker-push
-docker-push: docker-build
+.PHONY: docker-build-n
+docker-build-n: ;
+
+.PHONY: docker-push-y
+docker-push-y: docker-build-y
 	docker push ${REPO}:${VERSION}
+	
+.PHONY: docker-push-n
+docker-push-n: ;
 
-.PHONY: docker-prerequisites
-docker-prerequisites:
+.PHONY: docker-prerequisites-y
+docker-prerequisites-y:
 	[ -n "$(REPO)" ] || (echo "docker repository is not defined; please create the REPO file" && false)
-
-
